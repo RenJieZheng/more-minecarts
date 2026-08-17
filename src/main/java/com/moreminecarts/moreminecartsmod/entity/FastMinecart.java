9c1261bfcf998957c8ed43ce75fbbf6ad966ec2d
RenJieZheng <renjie_zheng@outlook.com>
2026-08-16 21:15:50 -0400

    Increased fast minecart speed to 32 blocks/s

--- src/main/java/com/moreminecarts/moreminecartsmod/entity/FastMinecart.java
+++ src/main/java/com/moreminecarts/moreminecartsmod/entity/FastMinecart.java
@@ -1,11 +1,39 @@
 package com.moreminecarts.moreminecartsmod.entity;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.Minecart;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseRailBlock;
+import net.minecraft.world.level.block.PoweredRailBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.tags.BlockTags;
 
 public class FastMinecart extends Minecart{
     public FastMinecart(EntityType<? extends FastMinecart> type, Level level) {
         super(type, level);
+    }
+
+    @Override
+    public float getMaxCartSpeedOnRail() {
+        return 1.6F;
+    }
+
+    @Override
+    public double getMaxSpeedWithRail() {
+        if (!this.canUseRail()) {
+            return this.getMaxSpeed();
+        } else {
+            BlockPos pos = this.getCurrentRailPosition();
+            BlockState state = this.level().getBlockState(pos);
+            if (!state.is(BlockTags.RAILS)) {
+                return this.getMaxSpeed();
+            } else {
+                float railMaxSpeed = this.isInWater() ? 0.8F : 1.6F;
+                return (double)Math.min(railMaxSpeed, this.getCurrentCartSpeedCapOnRail());
+            }
+        }
     }
 }
